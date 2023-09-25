89b53f4b28bed9cec721651651f2e4846e47c0fc
cydorespawn <cstine@mydrivesmart.com>
2023-09-25 10:20:54 -0400

    Added JWT tokens and refresh tokens

--- resources/item.py
+++ resources/item.py
@@ -1,68 +1,68 @@
-import uuid
-from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import items
+from sqlalchemy.exc import SQLAlchemyError
+from flask_jwt_extended import jwt_required, get_jwt
+
+from db import db
+from models import ItemModel
+from schemas import ItemSchema, ItemUpdateSchema
+
+blp = Blueprint("Items", "items", description="Operations on items")
 
 
-blp = Blueprint("Items", __name__, description="Operations on items")
-
-
-@blp.route("/item/<string:item_id>")
+@blp.route("/item/<int:item_id>")
 class Item(MethodView):
+    @jwt_required()
+    @blp.response(200, ItemSchema)
     def get(self, item_id):
-        try:
-            return items[item_id]
-        except KeyError:
-            abort(404, message="Item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        return item
 
+    @jwt_required()
     def delete(self, item_id):
-        try:
-            del items[item_id]
-            return {"message": "Item deleted."}
-        except KeyError:
-            abort(404, message="Item not found.")
+        jwt = get_jwt()
+        if not jwt.get("is_admin"):
+            abort(401, message="Admin privledge required.")
+        item = ItemModel.query.get_or_404(item_id)
+        db.session.delete(item)
+        db.session.commit()
+        return {"message": "Item was deleted."}
 
-    def put(self, item_id):
-        item_data = request.get_json()
-        if "price" not in item_data or "name" not in item_data:
-            abort(
-                400, message="Please ensure that 'name' and 'price' are both set in the JSON payload.")
+    @jwt_required()
+    @blp.arguments(ItemUpdateSchema)
+    @blp.response(200, ItemSchema)
+    def put(self, item_data, item_id):
+        item = ItemModel.query.get(item_id)
 
-        try:
-            item = items[item_id]
-            item |= item_data
-            return item
+        if item:
+            item.price = item_data["price"]
+            item.name = item_data["name"]
+        else:
+            item = ItemModel(id=item_id, **item_data)
 
-        except KeyError:
-            abort(404, message="Item could not be found.")
+        db.session.add(item)
+        db.session.commit()
+
+        return item
 
 
+@blp.route("/item")
 class ItemList(MethodView):
+    @jwt_required()
+    @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return {"items": list(items.values())}
+        return ItemModel.query.all()
 
-    def post(self):
-        item_data = request.get_json()
-        if (
-            "price" not in item_data
-            or "store_id" not in item_data
-            or "name" not in item_data
-        ):
-            abort(400, message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.")
+    @jwt_required(fresh=True)
+    @blp.arguments(ItemSchema)
+    @blp.response(201, ItemSchema)
+    def post(self, item_data):
+        item = ItemModel(**item_data)
 
-        for item in items.values():
-            if (
-                item_data["name"] == item["name"]
-                and item_data["store_id"] == item["store_id"]
-            ):
-                abort(400, message="Item already exists.")
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            abort(500, message=str(e))
 
-        if item_data["store_id"] not in stores:
-            abort(404, message="Store not found.")
-
-        item_id = uuid.uuid4().hex
-        item = {**item_data, "id": item_id}
-        items[item_id] = item
-
-        return item, 201
+        return item
