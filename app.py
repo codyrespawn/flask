87ac5fda540a7f3fcdcf311a4751fd7278f9e4c7
cydorespawn <cstine@mydrivesmart.com>
2023-09-25 10:43:13 -0400

    Added DB migrations

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import os
 import secrets
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 
 from db import db
 from blocklist import BLOCKLIST
@@ -25,11 +27,12 @@ def create_app(db_url=None):
     app.config[
         "OPENAPI_SWAGGER_UI_URL"
     ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv(
+        "DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["PROPAGATE_EXCEPTIONS"] = True
     db.init_app(app)
-
+    migrate = Migrate(app, db)
     api = Api(app)
 
     app.config["JWT_SECRET_KEY"] = "236174589647208174677740873385749648486"
@@ -39,13 +42,16 @@ def create_app(db_url=None):
     def check_if_token_in_blocklist(jwt_header, jwt_payload):
         return jwt_payload["jti"] in BLOCKLIST
 
-    @jwt.revoked_token_loader
-    def revoked_token_clalback(jwt_header, jwt_payload):
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_header, jwt_payload):
         return (
             jsonify(
-                {"description": "The token has been revoked.",
-                    "error": "token_revoked"}
-            ), 401
+                {
+                    "description": "The token is not fresh.",
+                    "error": "fresh_token_required",
+                }
+            ),
+            401,
         )
 
     @jwt.needs_fresh_token_loader
@@ -92,8 +98,8 @@ def create_app(db_url=None):
             ), 401
         )
 
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    #     db.create_all()
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
